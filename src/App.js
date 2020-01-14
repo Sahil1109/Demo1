a1587281413adc0e15a562ad1245b6a7175828d5
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-14 10:44:32 +0530

    Commit after Fwd refs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <FRParentInput/>
     </div>
   );
 }
