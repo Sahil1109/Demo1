aaf92e3611df728c119b09607229564912aa82cd
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-15 14:11:18 +0530

    Commit after HTTP POST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
-   <PostList></PostList>
+    
+    <PostForm></PostForm>
+   {/* <PostList></PostList> */}
     </div>
   );
 }
