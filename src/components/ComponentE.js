68b44697b13700c15a0d5bfed84145cd81886541
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-15 10:13:34 +0530

    Commit after Context(2)

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 class ComponentE extends Component {
     render() {
         return (
             <div>
-                <ComponentF/>
-            </div>
-        )
+               Component E context {this.context}
+               <ComponentF/>
+            </div>)
     }
 }
+ComponentE.contextType=UserContext
 
 export default ComponentE
